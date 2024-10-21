e4a9975fd7f724df46b389ecc58fbfc8fea2a360
Lucas <lucasferminnieto@yahoo.com.ar>
2024-10-21 13:29:38 -0300

    agregamos funcion fecha

---
+++ utils.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+def fechaActual():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+#%Y-%m-%d %H:%M:%S Formatea la fecha en un formato las legible
+#Año-Mes-Dia Hora:Minutos
+print((fechaActual))
