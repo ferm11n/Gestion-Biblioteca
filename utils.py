8bdc3a1c1448a17adb345a3807ef9a54770026a3
Lucas <lucasferminnieto@yahoo.com.ar>
2024-10-21 13:30:28 -0300

    Elimine el print a fechaActual

--- utils.py
+++ utils.py
@@ -4,4 +4,3 @@ def fechaActual():
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 #%Y-%m-%d %H:%M:%S Formatea la fecha en un formato las legible
 #Año-Mes-Dia Hora:Minutos
-print((fechaActual))
